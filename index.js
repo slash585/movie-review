f8d99d4bed22659dd79e0e2bfd22ab1cb65b6ed0
slash585 <51036040+slash585@users.noreply.github.com>
2021-01-18 14:10:01 +0300

    Create user and index routes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const {mongoose} = require('./bootstrap')
+const indexRouter = require('./routes/index')
+const userRouter = require('./routes/user')
+
+
+app.use('/',indexRouter)
+app.use('/user',userRouter)
+
 
 
 module.exports = app
